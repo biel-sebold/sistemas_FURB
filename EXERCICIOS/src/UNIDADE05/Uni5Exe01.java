786c9a84673e8e67356009e5d6d0d118401e13b0
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-04-22 19:57:19 -0300

    A

---
+++ EXERCICIOS/src/UNIDADE05/Uni5Exe01.java
@@ -0,0 +1,29 @@
+package EXERCICIOS.src.UNIDADE05;
+
+import java.util.Scanner;
+
+public class Uni5Exe01 {
+    public static void main(String[] args) {
+       //DANTON
+       //Descreva um algoritmo que leia 20 números inteiros (todos até 20) e escreva, para cada número lido, se o mesmo for par ou ímpar.
+
+        Scanner sc = new Scanner(System.in);   
+
+        for(int i = 1; i <= 20; i++){
+            System.out.printf("Informe o número %d: ", i);          
+            int numero = sc.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println("Número par");
+                
+            } else{
+                System.out.println("Número ímpar");
+            }
+
+            // System.out.printf("O número %d é %s%n", i, numero % 2 == 0 ? "par" : "ímpar");//USOU TERNÁRIOS
+        }
+
+        sc.close();
+    }
+
+}
---
+++ ExemplosAula/src/RepeticaoFor.java
@@ -0,0 +1,79 @@
+package src;
+
+// import java.util.Scanner;
+
+public class RepeticaoFor {
+    public static void main(String[] args) {
+
+        //CONTANDO DE 0 A 5
+
+        // int inicio = 0;
+        // System.out.println(inicio++);
+        // System.out.println(inicio++);
+        // System.out.println(inicio++);
+        // System.out.println(inicio++);
+        // System.out.println(inicio++);
+        // System.out.println(inicio++);
+
+
+
+        //CONTANDO DE 0 A 5 COM FOR
+
+        for(int i = 0/*inicio */; i <= 5/*teste */; i++/*incrmento */){
+             System.out.println(i);
+
+        }
+
+
+
+
+        //CONTANDO DE 10 A 0 
+
+        // for(int i = 10/*inicio */; i >= 0/*teste */; i--/*decrmento */){
+        //     System.out.println(i);
+
+        // }
+
+
+
+        //!!DESAFIO!!
+        //  LEIA UM NÚMERO E ESCREVA SUA TABUADA
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Escreva um número: ");
+        // int num = sc.nextInt();
+
+        // for(int i = 0; i <= 10; i ++){
+        //     ////System.out.println(i);
+        //     System.out.printf("%d x %d = %d%n", num, i, (num * i));
+        // }
+
+        // sc.close();
+
+
+
+        //!!DESAFIO 2!!
+        //  LEIA TRÊS NÚMEROS ONDE  O PRIMEIRO É A TABUADA, O SEGUNDO O INÍCIO E O TERCEIRO O FIM
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Informe a tabuada: ");
+        // int tab = sc.nextInt();
+
+        // System.out.println("Informe o início: ");
+        // int ini = sc.nextInt();
+
+        // System.out.println("Informe o fim: ");
+        // int fim = sc.nextInt();
+
+        // for(int i = ini; i <= fim; i++){
+        //     System.out.printf("%d x %d = %d%n", tab, i, (tab * i));
+        // }
+
+        // sc.close();
+       
+        
+    // }
+
+    }
+}
+
