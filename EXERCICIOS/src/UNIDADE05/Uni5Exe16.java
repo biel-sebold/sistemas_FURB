cb5d57554dacfffeabf56a1e5ad871c0f09c87f3
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-05-06 20:03:22 -0300

    .

--- EXERCICIOS/src/UNIDADE05/Uni5Exe16.java
+++ EXERCICIOS/src/UNIDADE05/Uni5Exe16.java
@@ -1,8 +1,21 @@
 package EXERCICIOS.src.UNIDADE05;
 
+import java.util.Scanner;
+
 public class Uni5Exe16 {
     public static void main(String[] args) {
         //Dado um determinado grupo de pessoas, descreva um algoritmo que a partir da altura e do gênero (gênero = 'M' ou gênero = 'm' para masculino; gênero = 'F' e gênero = 'f' para feminino ou gênero = 'O' ou gênero = 'o' para outro) de cada pessoa informe a média da altura das mulheres e a média de altura do grupo. A leitura deve ser finalizada ao digitar 0 para a altura.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o gênero: ");
+        char genero = sc.nextLine().charAt(0);
+
+        
+
+
+
+        sc.close();
     }
 
 }
