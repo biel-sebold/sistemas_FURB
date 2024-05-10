799a8a7112606c535eeb796051c2bab57a8676b4
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-05-09 21:29:52 -0300

    .

--- EXERCICIOS/src/UNIDADE05/Uni5Exe14.java
+++ EXERCICIOS/src/UNIDADE05/Uni5Exe14.java
@@ -12,27 +12,49 @@ public class Uni5Exe14 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Informe o nome: ");
-        String nome = sc.next();
-        System.out.print("Informe o preço de compra: R$");
-        double pc = sc.nextDouble();
-        System.out.print("Informe o preço de venda: R$");
-        double pv = sc.nextDouble();
+        int contador1 = 0;
+        int contador2 = 0;
+        int contador3 = 0;
+        double vcTotal = 0;
+        double vvTotal = 0;
 
-        double lucro = (pv - pc) / pc * 100;
-
-
-        for(int i = 1; i <= 20; i++) {
+        for(int i = 1; i <= 2; i++) {
 
             System.out.print("\nInforme o nome: ");
-            nome = sc.next();
+            String nome = sc.next();
             System.out.print("Informe o preço de compra: R$");
-            pc = sc.nextDouble();
+            double pc = sc.nextDouble();
             System.out.print("Informe o preço de venda: R$");
-            pv = sc.nextDouble();
-            
+            double pv = sc.nextDouble();
+
+            double lucro = (pv - pc) / pc * 100;
+
+            if (lucro < 10) {
+                contador1++;
+                
+            } else if (lucro <= 20) {
+                contador2++;
+
+            } else {
+                contador3++;
+            }
+
+            vcTotal += pc; //AJUDADO
+            vvTotal += pv; //AJUDADO
 
         }
+
+        double lucroTotal = vvTotal - vcTotal; //AJUDADO
+
+        System.out.printf("\n%d mercadorias proporcionaram lucro menor de 10%%\n", contador1);
+        System.out.printf("%d mercadorias proporcionaram lucro entre 10%% e 20%%\n", contador2);
+        System.out.printf("%d mercadorias proporcionaram lucro maior de 20%%\n\n", contador3);
+
+        System.out.printf("VALOR TOTAL DE COMPRA = R$%.2f\n", vcTotal);
+        System.out.printf("VALOR TOTAL DE VENDA = R$%.2f\n", vvTotal);
+        System.out.printf("LUCRO TOTAL = R$%.2f", lucroTotal);
+
+
 
         sc.close();
 
