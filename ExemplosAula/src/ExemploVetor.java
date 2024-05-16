ced3835c70ccfd4bc98972e026fbd159e75ed2e7
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-05-15 21:44:38 -0300

    .

---
+++ ExemplosAula/src/ExemploVetor.java
@@ -0,0 +1,71 @@
+package src;
+
+import java.util.Scanner;
+
+public class ExemploVetor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//==================================================================================================================
+//LÊ NÚMEROS E DEPOIS MOSTRA
+        // int numeros[] = new int[10];
+
+        // for(int i = 0; i < numeros.length; i++) {
+        //     System.out.printf("Informe n° %d: ", i );
+        //     numeros[i] = sc.nextInt();
+
+        // }
+
+        // for(int i = 0; i < numeros.length; i++) {
+        //     System.out.println(numeros[i]);
+        // }
+
+//==========================================================================================================   
+
+
+//ARMAZENAR SALARIOS PRÉ DECLARADOS E MOSTRA
+
+        // float[] salarios = new float[3];
+        // salarios[0] = (float) 1000.00; //EXEMPLO1 (melhor)
+        // salarios[1] = 1450.0F;//EXEMPLO2
+        // salarios[2] = (float) 10954.53;
+        
+        // for(int i = 0; i < salarios.length; i++) {
+        //     System.out.println(salarios[i]);
+
+        // }
+
+
+//==========================================================================================================
+
+//CRIE UM VETOR DE 50 POSIÇÕES, PREENCHA COM NÚMEROS ALEATÓRIOS (0 a 1000) E ACHE O MAIOR 
+
+        // int numeros[] = new int[50];
+
+        // for(int i = 0; i < numeros.length; i++) {
+        //     numeros[i] = (int)(Math.random() * 1001);
+
+        // }
+
+        // int maior = Integer.MIN_VALUE;
+
+        // for(int i = 0; i < numeros.length; i++) {
+
+        //     if (numeros[i] > maior) {
+        //         maior = numeros[i];
+                
+        //     }
+        // }
+
+        // System.out.printf("Maior número: %d\n", maior);
+
+        // for(int numero : numeros) {
+        //     System.out.printf("%d, ", numero );
+        // }
+
+            
+        sc.close();    
+
+    }
+
+}
--- ExemplosAula/src/variaveis.java
+++ ExemplosAula/src/variaveis.java
@@ -5,24 +5,37 @@ public class variaveis {
         // TIPOS PRIMITIVOS: (8)
         //inteiros (4)
         int variavelInteira = 1;
+        System.out.println(variavelInteira);
+
         byte variavelByte = 0;
+        System.out.println(variavelByte);
+
         short variavelShort = 0;
+        System.out.println(variavelShort);
+
         long variavelLong = 0;
+        System.out.println(variavelLong);
 
         //reais
         double varivelDouble = 0;
+        System.out.println(varivelDouble);
+
         float variavelFloat = 0;
+        System.out.println(variavelFloat);
 
         //lógico (1)
         boolean variavelLogica = true;
+        System.out.println(variavelLogica);
 
         //caracter (1)
         char variavelCaracter = 'A';
+        System.out.println(variavelCaracter);
 
 
         // ISSO NÃO É UM TIPO PRIMITIVO
         //cadeia de caracteres
         String variavelCadeiaCaracteres = "Gabriel";
+        System.out.println(variavelCadeiaCaracteres);
 
 
 
