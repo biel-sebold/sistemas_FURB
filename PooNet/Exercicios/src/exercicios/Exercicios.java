24c32687364293e5ee711472f162a257a8c86284
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-09-04 19:49:08 -0300

    .

--- PooNet/Exercicios/src/exercicios/Exercicios.java
+++ PooNet/Exercicios/src/exercicios/Exercicios.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package poonet;
+package exercicios;
 
 /**
  *
  * @author Almir
  */
-public class PooNet {
+public class Exercicios {
 
     /**
      * @param args the command line arguments
