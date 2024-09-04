4980f5c0bbb723764d78ee27c25110c14b2d58ed
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-09-04 19:35:41 -0300

    .

---
+++ PooNet/test/poonet/PooNetTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package poonet;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Almir
+ */
+public class PooNetTest {
+    
+    public PooNetTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
